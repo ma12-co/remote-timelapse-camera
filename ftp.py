d23343eb930ed0a7fd50008315cb2920fb3d8140
mrc <marcoambrosini@pm.me>
2019-04-10 11:24:01 +0200

    ftp script updated

--- ftp.py
+++ ftp.py
@@ -1,11 +1,25 @@
-import ftplib
+#CHANGELOG: Uploads all files in the folder
 
-# Open FTP session
-session = ftplib.FTP('mrcmrc.ddns.net','pi','esteesuntimelapsemuylargo')
-# file to send
-file = open('marcovio.jpg','rb')                  
-# send the file
-session.storbinary('STOR '+'marcovio.jpg', file)     
-# close file and FTP
-file.close()                                    
-session.quit()
+import ftplib
+import os
+
+
+#ftp connection (server, user,pass)
+ftp = ftplib.FTP('mrcmrc.ddns.net','pi','esteesuntimelapsemuylargo')
+
+#folder from which to upload
+template_dir = '/home/pi/ftp/files/'
+
+#iterate through dirs and files 
+for root, dirs, files in os.walk(template_dir, topdown=True):
+    relative = root[len(template_dir):].lstrip(os.sep)
+    #enters dirs 
+    for d in dirs:
+        ftp.mkd(os.path.join(relative, d))
+    #uploads files
+    for f in files:
+        ftp.cwd(relative)
+        ftp.storbinary('STOR ' + f, open(os.path.join(template_dir, relative, f), 'rb'))
+        ftp.cwd('/')
+
+ftp.quit()
